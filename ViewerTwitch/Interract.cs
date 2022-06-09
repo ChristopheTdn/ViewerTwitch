a7f89835b259672de2b27c85bf7690122fdd5454
ToF <Christophe.taudin@gmail.com>
2022-06-09 08:18:33 +0200

    Gestion MENU Rafraichissement Manuel et Ouverture repertoire

---
+++ ViewerTwitch/Interract.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace ViewerTwitch
+{
+    public class Interract
+    {
+        private string localDir = "";
+        
+
+        // ***************************
+        // * CONSTRUCTEUR PAR DEFAUT *
+        // ***************************
+        public Interract()
+        {
+           localDir = Fnc_FindLocalDir() + @"\";
+            Core_Session();
+        }
+        public Interract(ConsoleKeyInfo input)
+        {
+            localDir = Fnc_FindLocalDir() + @"\";
+            if (input.Key == ConsoleKey.F1)
+            { OpenPlanningTxt(); }
+            if (input.Key == ConsoleKey.F2)
+            { openDataDir(); }
+        }
+
+        // **********************
+        // *         CORE       *
+        // **********************
+        private void Core_Session()
+        {
+        }
+
+
+        private void OpenPlanningTxt()
+        {
+            string rep = Directory.GetCurrentDirectory();
+            try
+            {
+                Process.Start("notepad.exe", rep + @"\planning.txt");
+
+            }
+            catch { }
+        }
+
+        private void openDataDir()
+        {
+            //ouvre le repertoire DATA
+            string link = localDir + "data";
+            Process.Start("explorer.exe", link);
+        }
+        private string Fnc_FindLocalDir()
+        {
+            // renvois le repertoire locale de l application
+            // pour retrouver les fichiers .TXT
+            string path = Directory.GetCurrentDirectory();
+            return path;
+        }
+    }
+}
--- ViewerTwitch/Program.cs
+++ ViewerTwitch/Program.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Text.Json;
 using System.Net;
 using System.Timers;
+using System.Diagnostics;
 
 namespace ViewerTwitch
 {
@@ -18,19 +19,40 @@ namespace ViewerTwitch
             myTimer.Enabled = true;
 
             Console.WriteLine("Le script est lancé");
+            Console.WriteLine("\n[F1]: Ouvre Planning.txt  [F2]: Ouvre Data Dir   [F5] : Refresh manuel\n");
             SessionSpartiate spartiate = new SessionSpartiate();
             ConsoleKeyInfo input;
             do
             {
                 input = Console.ReadKey();
-                if (input.Key == ConsoleKey.R)
+                if (input.Key == ConsoleKey.F5)
                 { spartiate = new SessionSpartiate(); }
+                if (input.Key == ConsoleKey.F1)
+                { 
+                    Interract interract = new Interract(input); 
+                }
+                if (input.Key == ConsoleKey.F2)
+                {
+                   Interract interract = new Interract(input);
+                }
+
             } while (input.Key != ConsoleKey.Escape);
         }
 
         // Définition Fonctions Principales
+        private static void OpenPlanningTxt()
+        {
+            string rep = Directory.GetCurrentDirectory();
+            try
+            {
+                Process.Start("notepad.exe",rep + @"\planning.txt");
+                
+            }
+            catch { }   
+        }
         private static void OnTimedEvent(object source, ElapsedEventArgs e)
         {
+            Console.WriteLine("\n[F1]: Ouvre Planning.txt  [F2]: Ouvre Data Dir  [F5] : Refresh manuel\n");
             try
             {
                 SessionSpartiate spartiate = new SessionSpartiate();
